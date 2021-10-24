064527a7eba2e5b9f1732d4a2a834573005555ad
falduti90 <85851263+falduti90@users.noreply.github.com>
2021-10-24 01:15:09 -0300

    24/10

--- Sistema de Gestion/include/Buques.h
+++ Sistema de Gestion/include/Buques.h
@@ -1,15 +1,40 @@
 #ifndef BUQUES_H
 #define BUQUES_H
+#include "Terminal.h"
 
 
 class Buques
 {
     public:
         Buques();
+        void setIdBuque(int NuevoId);
+        void setnombreBuque(char* NuevoNombre);
+        void setBanderaBuque(char *NuevaBandera);
+        void setNumeroViaje(int NuevoNro);
+        void setGiro(Terminal NuevoGiro);
+        void setActivo(bool NuevoEstado);
+        int getIdBuque();
+        char* getnombreBuque();
+        char* getBanderaBuque();
+        int getNumeroViaje();
+        Terminal getGiro();
+        bool getActivo();
+        void cargar();
+        void mostrar();
+        bool leerDeDisco(int);
+        bool grabarEnDisco();
+        bool grabarEnDisco(int);
 
-    protected:
+
 
     private:
+
+    int         _idBuque;
+    char        _nombreBuque[30];
+    char        _banderaBuque [30];
+    int         _numeroViaje;
+    Terminal    _giro;
+    bool        _activo;
 };
 
 #endif // BUQUES_H
--- Sistema de Gestion/src/Buques.cpp
+++ Sistema de Gestion/src/Buques.cpp
@@ -1,4 +1,72 @@
 #include <iostream>
 using namespace std;
 #include <cstdlib>
+#include <cstring>
+#include "Buques.h"
+#include "Terminal.h"
 
+        //Buques::Buques(){}
+        void Buques::setIdBuque(int NuevoId){
+            _idBuque=NuevoId;
+        }
+        void Buques::setnombreBuque(char* NuevoNombre){
+           strcpy(_nombreBuque,NuevoNombre);
+        }
+        void Buques::setBanderaBuque(char *NuevaBandera){
+            strcpy(_banderaBuque,NuevaBandera);
+        }
+
+        void Buques::setNumeroViaje(int NuevoNro){
+            _numeroViaje=NuevoNro;
+        }
+        void Buques::setGiro(Terminal NuevoGiro){
+            _giro=NuevoGiro;
+        }
+        void Buques::setActivo(bool NuevoEstado){
+            _activo=NuevoEstado;
+        }
+        int  Buques::getIdBuque(){
+            return _idBuque;
+        }
+        char *Buques::getnombreBuque(){
+            return _nombreBuque;
+        }
+        char *Buques::getBanderaBuque(){
+            return _banderaBuque;
+        }
+        int  Buques::getNumeroViaje(){
+            return _numeroViaje;
+        }
+        Terminal  Buques::getGiro(){
+
+        }
+        bool Buques::getActivo(){
+            return _activo;
+        }
+        void Buques::cargar(){
+            cout<< "INGRESE EL ID DEL BUQUE : "<<endl;
+            cin >> _idBuque;
+            cout<< "INGRESE EL NOMBRE DEL BUQUE :"<<endl;
+            cin >> _nombreBuque;
+            cout<< "INGRESE LA BANDERA DEL BUQUE :"<<endl;
+            cin >> _banderaBuque;
+            cout<< "INGRESE EL NUMERO DE VIAJE :"<<endl;
+            cin >> _numeroViaje;
+            cout<< "INGRESE EL GIRO : "<<endl;
+            cin>>  _giro;
+
+            _activo=true;
+
+
+        }
+        void Buques::mostrar(){
+            cout<< "NUMERO DE ID BUQUE-:"<< "/"<< _idBuque<<endl;
+            cout<< "NOMBRE DEL BUQUE---:"<< "/"<< _nombreBuque<<endl;
+            cout<< "BANDERA------------:"<< "/"<< _banderaBuque<<endl;
+            cout<< "NUMERO DE VIAJE----:"<< "/"<< _numeroViaje<<endl;
+            cout<< "GIRO---------------:"<< "/"<<_giro<<endl;
+
+        }
+        bool Buques::leerDeDisco(int){}
+        bool Buques::grabarEnDisco(){}
+        bool Buques::grabarEnDisco(int){}
