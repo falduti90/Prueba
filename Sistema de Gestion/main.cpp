e4cc7fc9052d453639d9b021bad2f07b55f9de4f
cataAvi <avilarotelacatalina@gmail.com>
2021-10-30 01:55:52 -0300

    Funciones Importar - Exportar Prueba
    Agrego dos funciones globales para importar y exportar informacion en archivos .CSV (faltan mejoras)

--- Sistema de Gestion/main.cpp
+++ Sistema de Gestion/main.cpp
@@ -3,7 +3,6 @@
 #include <ctime>
 #include <string.h>
 #include "Fecha.h"
-using namespace std;
 #include <cstring>
 #include "Buque.h"
 #include "Cronograma.h"
@@ -12,15 +11,23 @@ using namespace std;
 #include "Terminal.h"
 #include "Agencia.h"
 #include "Fecha.h"
+#include <sstream> //Libredias para
+#include <fstream> // el importador
+#define NOMBRE_ARCHIVO "Prueba Importador.csv" // Archivo para importador
+using namespace std;
+
+void importar();
+void exportar();
 
 
 
 int main(){
-    Cronograma reg;
+    //Cronograma reg;
 
     /*reg.cargar();
     cout << endl;
     reg.mostrar();*/
+    /*
 
     ListadoCronograma();
 
@@ -33,9 +40,41 @@ int main(){
     //strcat_
     cout << prueba1;
 
+    */
+
+    //importar();
+    exportar();
+
       return 0;
 }
 
+
+void importar(){
+    ifstream archivo(NOMBRE_ARCHIVO);
+	string linea;
+	char delimitador = ',';
+	getline(archivo, linea);
+
+	while(getline(archivo, linea)){
+        stringstream stream (linea);
+        string dia, mes, anio;
+        getline(stream, dia, delimitador);
+        getline(stream, mes, delimitador);
+        getline(stream, anio, delimitador);
+        cout << endl << dia << "/" << mes << "/" << anio;
+	}
+
+}
+
+void exportar(){
+    ofstream myFile;
+	myFile.open("test.csv");
+	for (int i = 0; i<20; i++){
+        myFile << i << ',' << i*i << endl;
+
+	}
+
+}
 
 
 
